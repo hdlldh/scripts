22be3b020d768d41a7cf1ac71ac9bc5d4c591654
donglin <hdlldh@gmail.com>
2021-06-17 10:56:08 -0700

    update

--- almond_scala.sh
+++ almond_scala.sh
@@ -1,4 +1,5 @@
 curl -Lo coursier https://git.io/coursier-cli
 chmod +x coursier
 ./coursier launch almond:0.10.9 --scala 2.12.12 -- --install --id scala212 --display-name "Scala (2.12)"
+#./coursier launch almond:0.10.9 --scala 2.13.6 -- --install --id scala213 --display-name "Scala (2.13)"
 rm -f coursier
