50c4bae9334468f868c5da6db8cc7dc560b03952
donglin <hdlldh@gmail.com>
2021-11-27 00:15:43 -0800

    fix java home path

--- java_openjdk_mac_linux.sh
+++ java_openjdk_mac_linux.sh
@@ -21,15 +21,16 @@ echo '# >>> java configuration >>>' >> $BASH_FILE
 
 if [ "$machine" = "Mac" ]; then
 	INSTALLER=OpenJDK${JAVA_MAJOR_VER}U-jdk_x64_mac_hotspot_${JAVA_MAJOR_VER}u${JAVA_MINOR_VER}b${JAVA_BIN_VER}.tar.gz
+	echo "export JAVA_HOME=${TOOLS_HOME}/${JAVA_FOLDER}/Contents/Home" >> $BASH_FILE
 else
 	INSTALLER=OpenJDK${JAVA_MAJOR_VER}U-jdk_x64_linux_hotspot_${JAVA_MAJOR_VER}u${JAVA_MINOR_VER}b${JAVA_BIN_VER}.tar.gz
+	echo "export JAVA_HOME=${TOOLS_HOME}/${JAVA_FOLDER}" >> $BASH_FILE
 fi
 echo https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/${JAVA_FOLDER}/${INSTALLER}
 
 curl -L -O https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/${JAVA_FOLDER}/${INSTALLER}
 tar -xvzf $INSTALLER
 mv ${JAVA_FOLDER} $TOOLS_HOME
-echo "export JAVA_HOME=${TOOLS_HOME}/${JAVA_FOLDER}" >> $BASH_FILE
 
 echo 'export JAVA_OPTS="$JAVA_OPTS -Duser.timezone=GMT"' >> $BASH_FILE
 echo 'export PATH=${JAVA_HOME}/bin:$PATH' >> $BASH_FILE
