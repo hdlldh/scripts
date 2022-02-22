cc6d804453606c868552f7bb092501ba76335c1d
donglin <hdlldh@gmail.com>
2022-02-21 22:04:45 -0800

    Add nlp hugging face

--- github_pull_all.sh
+++ github_pull_all.sh
@@ -1,4 +1,15 @@
-declare -a repos=("breeze" "catboost" "handson-ml2" "spark" "spark-nlp" "spark-nlp-workshop" "tensorflow" "tensorflow_java" "tf-deploy")
+declare -a repos=(\
+	"breeze" \
+	"catboost" \
+	"handson-ml2" \
+	"nlp_hugging_face" \
+	"spark" \
+	"spark-nlp" \
+	"spark-nlp-workshop" \
+	"tensorflow" \
+	"tensorflow_java" \
+	"tf-deploy"\
+) 
 
 for repo in "${repos[@]}"
 do
