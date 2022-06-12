6833f45bb4126339657bb31de45bfe438a3503f6
donglin <hdlldh@gmail.com>
2022-06-12 12:18:52 -0700

    add conda ml packages

---
+++ conda_install.sh
@@ -0,0 +1,9 @@
+# install pytorch
+conda install -c pytorch pytorch
+
+# install tensorflow
+# conda install -c anaconda tensorflow
+pip install tensorflow
+
+# install huggingface transformers
+conda install -c huggingface transformers
