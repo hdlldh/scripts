d49019fa67df2ce9b5c8e70e2c044d748a4c8eb5
hdlldh <hdlldh@gmail.com>
2022-10-07 00:28:46 -0700

    Update git_pulls

--- git_pulls.sh
+++ git_pulls.sh
@@ -1,38 +1,52 @@
 REPO_PATH="$HOME/Repos"
 [ ! -d "$REPO_PATH" ] && mkdir -p "$REPO_PATH"
 declare -a repo_urls=(\
-	"https://github.com/scalanlp/breeze" \
-	"https://github.com/catboost/catboost" \
+	"https://github.com/scalanlp/breeze.git" \
+	"https://github.com/catboost/catboost.git" \
 	"https://github.com/deepjavalibrary/d2l-java.git" \
 	"https://github.com/deepjavalibrary/djl.git" \
-	"https://github.com/deepjavalibrary/djl-demo" \
+	"https://github.com/deepjavalibrary/djl-demo.git" \
 	"https://github.com/ageron/handson-ml2.git" \
 	"https://github.com/huggingface/evaluate.git" \
-	"https://github.com/huggingface/tokenizers" \
-	"https://github.com/huggingface/transformers" \
-	"https://github.com/apache/spark" \
+	"https://github.com/huggingface/tokenizers.git" \
+	"https://github.com/huggingface/transformers.git" \
+	"https://github.com/apache/spark.git" \
 	"https://github.com/JohnSnowLabs/spark-nlp.git" \
 	"https://github.com/JohnSnowLabs/spark-nlp-workshop.git" \
-	"https://github.com/tensorflow/tensorflow" \
+	"https://github.com/tensorflow/docs.git" \
 	"https://github.com/tensorflow/tfx.git" \
 	"https://github.com/tensorflow/serving.git" \
 	"https://github.com/tensorflow/java.git" \
 	"https://github.com/pytorch/pytorch.git" \
 	"https://github.com/pytorch/serve.git" \
+	"https://github.com/pytorch/tutorials.git" \
 ) 
 
 for url in "${repo_urls[@]}"
 do
     repo=$(basename "$url" | cut -f 1 -d ".")
+    path=$(dirname "$url")
+    owner=$(basename "$path")
     echo "Checking $repo"
-    if [ -d "$REPO_PATH/$repo" ]; then
-        echo "Updating $repo"
-        cd "$REPO_PATH/$repo"
+    if [ $owner = "tensorflow" ]; then
+	renamed_repo="tf_$repo"
+    elif [ $owner = "pytorch" ]; then
+        renamed_repo="pt_$repo"
+    else
+	renamed_repo=$repo
+    fi 
+    if [ -d "$REPO_PATH/$renamed_repo" ]; then
+        echo "Updating $renamed_repo"
+        cd "$REPO_PATH/$renamed_repo"
         git pull
     else
         echo "$REPO_PATH/$repo does not exist."
         echo "Cloning $repo"
         cd $REPO_PATH
-        git clone $url
+        if [ $repo = $renamed_repo ]; then
+        	git clone $url
+	else
+		git clone $url $renamed_repo
+	fi
     fi
 done
